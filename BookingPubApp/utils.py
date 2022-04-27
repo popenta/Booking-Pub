443388103e24fc5dad53fe3b7411fd0e29c904ec
popenta <alexandru.p9913@gmail.com>
2022-04-27 18:07:03 +0300

    small code refactoring

---
+++ BookingPubApp/utils.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+import tensorflow as tf
+import xml.etree.ElementTree as ET
+
+
+def get_images_from_folder(folder: str):
+    images_jpg = []
+    images_xml = []
+    for img in os.listdir(folder):
+        if img.endswith('jpg'):
+            images_jpg.append(img)
+        elif img.endswith('xml'):
+            images_xml.append(img)
+    
+    return images_jpg, images_xml
+
+
+def get_subimages_from_image(img_dir, img_jpg, img_xml, number):
+    complete_img = []
+    tree = ET.parse(os.path.join(img_dir, img_xml[number]))
+    root = tree.getroot()
+
+    for member in root.findall('object'):
+        bndbox = member.find('bndbox')
+        xmin = int(bndbox.find('xmin').text)
+        ymin = int(bndbox.find('ymin').text)
+        xmax = int(bndbox.find('xmax').text)
+        ymax = int(bndbox.find('ymax').text)
+
+        img = tf.keras.preprocessing.image.load_img(os.path.join(img_dir, img_jpg[number]))
+        img_arr = tf.keras.preprocessing.image.img_to_array(img)
+
+        img_arr = img_arr[ymin:ymax, xmin:xmax]
+        img_arr = tf.image.resize(img_arr,(150, 150)).numpy()
+        complete_img.append(img_arr)
+    
+    X_array = np.asarray(complete_img, dtype='float32')
+    
+    return X_array
+
+
+def prediction(X_array, model):
+    suma = 0
+    for i in range(len(X_array)):
+        subimg = X_array[i]/255.
+        image = np.expand_dims(subimg, axis=0)
+        value = model.predict(image, verbose=1, batch_size = 1)
+        if value > 0.5:
+            suma += 1
+    
+    return suma
+
--- BookingPubApp/views.py
+++ BookingPubApp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from BookingPubApp.models import Restaurant
-import os
 import random
 import keras
-import numpy as np
-import xml.etree.ElementTree as ET
-import tensorflow as tf
+from .utils import *
+
+#loading the model
+trained_model = keras.Model()
+trained_model = keras.models.load_model("model_v1_1.h5")
+
 
 # Create your views here.
 def main_page(request):
@@ -22,52 +23,13 @@ def single_restaurant_page(request, id):
     dictionar["r"] = restaurant
     folder = restaurant.images
     
-    images_jpg = []
-    images_xml = []
-    for img in os.listdir(folder):
-        if img.endswith('jpg'):
-            images_jpg.append(img)
-        elif img.endswith('xml'):
-            images_xml.append(img)
+    images_jpg, images_xml = get_images_from_folder(folder)
     
     number = random.randint(0, len(images_jpg)-1)
     print(images_jpg[number])
 
-    complete_img = []
-    tree = ET.parse(os.path.join(restaurant.images, images_xml[number]))
-    root = tree.getroot()
-    for member in root.findall('object'):
-        bndbox = member.find('bndbox')
-        xmin = int(bndbox.find('xmin').text)
-        ymin = int(bndbox.find('ymin').text)
-        xmax = int(bndbox.find('xmax').text)
-        ymax = int(bndbox.find('ymax').text)
+    X_array = get_subimages_from_image(restaurant.images, images_jpg, images_xml, number)
 
-        img = tf.keras.preprocessing.image.load_img(os.path.join(restaurant.images, images_jpg[number]))
-        img_arr = tf.keras.preprocessing.image.img_to_array(img)
-
-        img_arr = img_arr[ymin:ymax, xmin:xmax]
-        img_arr = tf.image.resize(img_arr,(150, 150)).numpy()
-        complete_img.append(img_arr)
-    
-    X_array = np.asarray(complete_img, dtype='float32')
-
-    trained_model = keras.Model()
-    trained_model = keras.models.load_model("model_v1_1.h5")
-
-    #TODO adauga numar mese in model restaurant, si ca sa vezi cate sunt ocupate scazi nr mese - suma
-
-    suma = 0
-    for i in range(len(X_array)):
-        subimg = X_array[i]/255.
-        image = np.expand_dims(subimg, axis=0)
-        value = trained_model.predict(image, verbose=1, batch_size = 1)
-        if value > 0.5:
-            suma += 1
-
-    print(suma)
-    dictionar["tables"] = suma
-    print(dictionar)
-
+    dictionar["tables"] = prediction(X_array, trained_model)
 
     return render(request, 'single_restaurant.html', dictionar)
